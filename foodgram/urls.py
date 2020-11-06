724572f22d28a5eaa397509ae8476b14c1178235
Sergey Pogorelov <Serge.pogorelov@gmail.com>
2020-11-06 22:05:28 +0300

    include recipes.urls

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("auth/", include("users.urls")),
     path("auth/", include("django.contrib.auth.urls")),
+    path("", include("recipes.urls")),
 ]
 
 
