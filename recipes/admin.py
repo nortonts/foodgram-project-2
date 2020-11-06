6a6d845803bd6c253ec5fcdf74bf330603579cde
Sergey Pogorelov <Serge.pogorelov@gmail.com>
2020-11-06 13:44:27 +0300

    add recipes admin settings

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredients, IngredientValue, Recepie
+
+class IngredientValueInline(admin.StackedInline):
+    model = IngredientValue
+
+
+@admin.register(Recepie)
+class RecepieAdmin(admin.ModelAdmin):
+    inlines = [IngredientValueInline,]
+    prepopulated_fields = {"slug": ("name",)}
+    
+    list_display = ("name", "pub_date", "author")
+
+    fieldsets = (
+        (None, {'fields': ('author', 'name', 'slug')}),
+        ("Теги", {'fields': (("breakfast", "lunch", "dinner",),)}),
+        ("Информация и фото", {
+            'fields': ("сooking_time", "description", "image",),
+        }),
+    )
+
+    list_filter = (
+        "author",
+        "name",
+        "breakfast", 
+        "lunch", 
+        "dinner"
+    )
+
+    # TODO На странице рецепта вывести число добавлений этого рецепта в избранное.
+
+
+@admin.register(Ingredients)
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = ("name", "units")
+    # TODO Добавить фильтр по названию.
--- recipes/apps.py
+++ recipes/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class RecipesConfig(AppConfig):
-    name = 'recipes'
+    name = "recipes"
+    verbose_name = "Рецепты"
