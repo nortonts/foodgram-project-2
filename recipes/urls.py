e6cb8e2e066bcbea0382b2f488b2e769cdcd489b
Sergey Pogorelov <Serge.pogorelov@gmail.com>
2020-11-10 10:11:00 +0300

    add AuthorRecipeListView

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,7 +5,18 @@ from . import views
 
 urlpatterns = [
     path("", views.RecipeListView.as_view(), name="recipe_list"),
-    path("create/", views.RecipeCreateView.as_view(), name ="recipe_create"),
-    path("<slug>/edit/", views.RecipeUpdateView.as_view(), name ="recipe_edit"),
-    path("<slug>/", views.RecipeDetailView.as_view(), name ="recipe_detail"),
+    path(
+        "author/<str:username>",
+        views.AuthorRecipeListView.as_view(),
+        name="author_recipe_list",
+    ),
+    path("create/", views.RecipeCreateView.as_view(), name="recipe_create"),
+    path(
+        "<str:slug>/edit/",
+        views.RecipeUpdateView.as_view(),
+        name="recipe_edit",
+    ),
+    path(
+        "<str:slug>/", views.RecipeDetailView.as_view(), name="recipe_detail"
+    ),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -93,3 +93,20 @@ class RecipeUpdateView(LoginRequiredMixin, UpdateView):
             form.save()
             return redirect("recipe_detail", self.object.slug)
         return render(request, "recipes/recipe_form.html", {"form": form})
+
+
+class AuthorRecipeListView(ListView):
+    model = Recipe
+    paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["current_page"] = "recipe"
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        username = self.kwargs.get("username")
+        if username:
+            queryset = queryset.filter(author__username=username)
+        return queryset
