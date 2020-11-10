faaad13ecc7fc3408e1523975ff3b09def0a3344
Sergey Pogorelov <Serge.pogorelov@gmail.com>
2020-11-10 10:23:02 +0300

    add username at get_full_name func

--- users/models.py
+++ users/models.py
@@ -2,4 +2,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    
+    def get_full_name(self):
+        full_name = super().get_full_name()
+        if not full_name:
+            full_name = self.username
+        return full_name.strip()
