7a7d8adb0b949080cf58bf0ad81ce152d6892fae
Sergey Pogorelov <Serge.pogorelov@gmail.com>
2020-11-11 11:37:39 +0300

    move validation in serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
+from rest_framework.serializers import ValidationError
 
 from recipes.models import Ingredients, Subscription, Favorite
 
 
 class IngredientsSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Ingredients
         fields = ["title", "dimension"]
@@ -16,8 +17,31 @@ class SubscriptionSerializer(serializers.ModelSerializer):
         model = Subscription
         fields = ["id"]
 
+    def save(self, **kwargs):
+        user = kwargs.get("user")
+        author = kwargs.get("author")
+        if user == author:
+            raise ValidationError(
+                {"detail": "You can't subscribe to yourself"}
+            )
+        if Subscription.objects.filter(user=user, author=author).exists():
+            raise ValidationError(
+                {"detail": "You have already subscribed to this author"}
+            )
+        return super().save(**kwargs)
+
+
 class FavoriteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Favorite
         fields = ["id"]
+
+    def save(self, **kwargs):
+        user = kwargs.get("user")
+        recipe = kwargs.get("recipe")
+        if Favorite.objects.filter(user=user, recipe=recipe).exists():
+            raise ValidationError(
+                {"detail": "This recipe is already in your favorites"}
+            )
+        return super().save(**kwargs)
--- api/views.py
+++ api/views.py
@@ -6,10 +6,13 @@ from rest_framework.generics import (
     get_object_or_404,
 )
 from rest_framework.response import Response
-from rest_framework.serializers import ValidationError
 
 from recipes.models import Ingredients, Subscription, Favorite, Recipe
-from .serializers import IngredientsSerializer, SubscriptionSerializer, FavoriteSerializer
+from .serializers import (
+    IngredientsSerializer,
+    SubscriptionSerializer,
+    FavoriteSerializer,
+)
 
 
 User = get_user_model()
@@ -35,10 +38,6 @@ class SubscriptionCreateAPIView(CreateAPIView):
         user = self.request.user
         author_id = self.request.data.get("id")
         author = get_object_or_404(User, id=author_id)
-        if user == author:
-            raise ValidationError(
-                {"detail": "You can't subscribe to yourself"}
-            )
         serializer.save(user=user, author=author)
 
 
