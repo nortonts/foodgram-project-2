95f1ab6423859c6c3f2ddda76ad997a2bc035a7c
Sergey Pogorelov <Serge.pogorelov@gmail.com>
2020-11-13 01:10:50 +0300

    add autologin after registrations

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
@@ -6,5 +10,16 @@ from .forms import CreationForm
 
 class SignUp(CreateView):
     form_class = CreationForm
-    success_url = reverse_lazy("recipe_list")
     template_name = "signup.html"
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            user = form.save()
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password1"],
+            )
+            login(request, user)
+            return redirect("recipe_list")
+        return render(request, "signup.html", {"form": form})
