059d2a65c972b71b12381cbf3ad3d9b912685e18
Sergey Pogorelov <Serge.pogorelov@gmail.com>
2020-11-15 01:03:27 +0300

    refactoring ShoppingList

--- purchases/shoppinglist.py
+++ purchases/shoppinglist.py
@@ -9,21 +9,21 @@ class ShoppingList:
     def __init__(self, request):
         self.session = request.session
         shoppinglist = self.session.get(SHOPPINGLIST_SESSION_ID)
-        if not shoppinglist:
+        if shoppinglist is None:
             shoppinglist = self.session[SHOPPINGLIST_SESSION_ID] = []
         self.shoppinglist = shoppinglist
 
     def add(self, recipe_id):
-        if int(recipe_id) not in self.shoppinglist:
-            self.shoppinglist.append(int(recipe_id))
+        if recipe_id not in self.shoppinglist:
+            self.shoppinglist.append(recipe_id)
         self.save()
 
     def save(self):
         self.session.modified = True
 
     def remove(self, recipe_id):
-        if int(recipe_id) in self.shoppinglist:
-            self.shoppinglist.remove(int(recipe_id))
+        if recipe_id in self.shoppinglist:
+            self.shoppinglist.remove(recipe_id)
             self.save()
 
     def __len__(self):
@@ -34,7 +34,7 @@ class ShoppingList:
         recipes = Recipe.objects.filter(id__in=recipes_id)
         for recipe in recipes:
             yield recipe
-            
+
     def get_objects(self):
         return Recipe.objects.filter(id__in=self.shoppinglist)
 
@@ -50,15 +50,12 @@ class ShoppingList:
             recipe_ingridients = get_ingredients(recipe)
             for ingridient in recipe_ingridients:
                 title, value, dimention = ingridient
-                if ingridients_for_pdf.get(title):
-                    ingridients_for_pdf[title][0] += value.value
-                    ingridients_for_pdf[title][2].append(recipe.name)
-                else:
-                    ingridients_for_pdf[title] = [
-                        value.value,
-                        dimention,
-                        [recipe.name],
-                    ]
+
+                ingridients_for_pdf[title] = ingridients_for_pdf.get(
+                    title, [0, dimention, []]
+                )
+                ingridients_for_pdf[title][0] += value.value
+                ingridients_for_pdf[title][2].append(recipe.name)
 
         ingridients_for_pdf = [
             [title, value[0], value[1], value[2]]
